455e8462a0a6a6d94611daccc06f10540d9453e9
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-26 15:30:44 +0800

    ✨ NEW FEATURE: create add-element modal and table component (#5)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import {IoSearch} from 'react-icons/io5'
 import {IoMenu} from 'react-icons/io5'
 
 export default function NavBar() {
+
+  const router = useRouter()
+
   return (
-    <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+    <div className="navbar shadow-lg bg-neutral text-neutral-content">
       <div className="px-2 mx-2 navbar-start">
         <span className="text-lg font-bold">
           Samp<span className="text-gray-400">Ag</span>uita
@@ -13,17 +17,17 @@ export default function NavBar() {
       <div className="navbar-center hidden px-2 mx-2 lg:flex">
         <div className="flex items-stretch space-x-2">
           <Link href="/">
-            <a className="btn btn-ghost btn-sm rounded-btn">
+            <a className={`${router.pathname === '/' ? 'btn-active' : 'btn-ghost'} btn btn-ghost btn-sm rounded-btn`}>
               Home
             </a> 
           </Link>
           <Link href="/periodic-table">
-            <a className="btn btn-ghost btn-sm rounded-btn">
+            <a className={`${router.pathname === '/periodic-table' ? 'btn-active' : 'btn-ghost'} btn btn-ghost btn-sm rounded-btn`}>
               Periodic Table
             </a> 
           </Link>
           <Link href="/">
-            <a className="btn btn-ghost btn-sm rounded-btn">
+            <a className={`${router.pathname === '/about' ? 'btn-active' : 'btn-ghost'} btn btn-ghost btn-sm rounded-btn`}>
               About
             </a> 
           </Link>
